108ba59260f883d62436341311ac6434502cdd32
Samuel Yanez <samuelyanez94@gmail.com>
2026-08-16 19:08:22 -0700

    Configure SwiftData model container

--- ChatLLM/App/ChatLLMApp.swift
+++ ChatLLM/App/ChatLLMApp.swift
@@ -4,13 +4,28 @@
 //
 //  Created by Samuel Yanez on 8/14/26.
 
+import SwiftData
 import SwiftUI
 
 @main
 struct ChatLLMApp: App {
+	private let modelContainer: ModelContainer
+
+	init() {
+		do {
+			modelContainer = try ModelContainer(
+				for: Schema(versionedSchema: ChatSchemaV1.self),
+				migrationPlan: ChatMigrationPlan.self
+			)
+		} catch {
+			fatalError("Failed to create the chat model container: \(error)")
+		}
+	}
+
 	var body: some Scene {
 		WindowGroup {
 			ChatListView()
 		}
+		.modelContainer(modelContainer)
 	}
 }
